a59a1d35e6bc0253180c7b058fbbe0b0012e0d55
muhammad-jiku <muhammadjiku364@gmail.com>
2024-04-22 12:38:23 +0600

    task1 second q&a added

--- src/task1.ts
+++ src/task1.ts
@@ -13,3 +13,28 @@ console.log(userInfo);
 userInfo[0] = 1;
 console.log(userInfo[0]);
 console.log(userInfo);
+
+console.log('-------------------------------');
+
+// 2. Write a TypeScript function that takes in two arrays of numbers as parameters. The function should compare the elements in both arrays and return a new array that contains only the elements that are present in both arrays.
+
+// For example, if the first array is [1, 2, 3, 4, 5] and the second array is [2, 4, 6, 8], the function should return a new array with the elements 2 and 4 because they are present in both arrays.
+
+// The function should handle arrays of any length and should return the resulting array in the same order as they appear in the first array.
+
+function filterdArray(a1: number[], a2: number[]): number[] {
+  const a2Mirror = new Set(a2);
+  const newArr = a1.filter((x) => a2Mirror.has(x));
+
+  return newArr;
+}
+
+const arr1: number[] = [1, 2, 3, 33, 45, 465, 700];
+const arr2: number[] = [3, 946, 63, 9896, 700];
+
+const arr3 = filterdArray(arr1, arr2);
+console.log('array 1 is..', arr1);
+console.log('array 2 is..', arr2);
+console.log('array 3 is..', arr3);
+
+console.log('-------------------------------');
